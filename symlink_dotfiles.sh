14b42f6de4e600256a18c930cc69d03ff8cc349f
Bui Chi Trung <trungcspntl@gmail.com>
2024-04-08 23:01:42 +0700

    ignore zshrc bashrc add alias

--- symlink_dotfiles.sh
+++ symlink_dotfiles.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
-function symlink_dotfiles() {
-  for i in $(find ~+ -maxdepth 1 -name '.*' ! -name '.DS_Store' ! -name '.gitignore' -type f)
-  do
-    echo "$i"
-    ln -sf "$i" ~
-  done
-}
+# function symlink_dotfiles() {
+#   for i in $(find ~+ -maxdepth 1 -name '.*' ! -name '.DS_Store' ! -name '.gitignore' -type f)
+#   do
+#     echo "$i"
+#     ln -sf "$i" ~
+#   done
+# }
 
-symlink_dotfiles
+# symlink_dotfiles
+ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
+ln -sf ~/dotfiles/.vimrc ~/.vimrc
